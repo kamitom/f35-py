4014b8e42288a50a5afe943bbcf630a1a3c8156d
ymjoz <rossiblack@duck.com>
2024-11-27 09:53:52 +0800

    initial commit: 使用black-version API

---
+++ test1.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+
+
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",  # gpt-3.5-turbo gpt-4o-mini
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "我想知道2000年後的世界杯足球賽的歷年棒球冠軍名單."
+        }
+    ],
+    max_tokens=500
+)
+
+print(completion.choices[0].message)
