dc4de88dcc5553d699b850d3704cbf6f5b870fa7
ymjoz <rossiblack@duck.com>
2024-12-04 16:59:57 +0800

    feat: 新增ChatOpenAI的功能

--- test1.py
+++ test1.py
@@ -9,10 +9,10 @@ completion = client.chat.completions.create(
         {"role": "system", "content": "You are a helpful assistant."},
         {
             "role": "user",
-            "content": "我想知道2000年後的世界杯足球賽的歷年冠軍名單."
+            "content": "我想知道2000年後的世界棒球經典賽的歷年冠軍/亞軍名單以及冠軍賽的舉辦地點體育場館名稱."
         }
     ],
-    max_tokens=500
+    max_tokens=899
 )
 
 print(completion.choices[0].message)
